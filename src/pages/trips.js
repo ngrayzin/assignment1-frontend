afb3ddf91e6ccd6593d5c901142a035b34ee0b53
ngrayzin <rayzin147@gmail.com>
2023-12-13 22:40:28 +0800

    remove brackets

--- src/pages/trips.js
+++ src/pages/trips.js
@@ -159,7 +159,7 @@ const Trips = () => {
             <div className="space-y-4">
                 {Array.isArray(trips) && trips.length > 0 ? (
                     trips
-                    .filter(trip => trip.isActive && !(trip.isStarted) && !(trip.isCancelled) && trip.availableSeats > 0)
+                    .filter(trip => trip.isActive && !trip.isStarted && !trip.isCancelled && trip.availableSeats > 0)
                     .map((trip, index) => (
                         <div key={trip.tripID} className="bg-white p-4 rounded-xl shadow-sm w-full">
                             <div className='flex items-center mb-1'>
